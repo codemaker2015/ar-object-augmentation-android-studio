7ce56a2498a1fddd5161ddb6a5882c18467aaa8c
Vishnu Sivan <codemaker2015@gmail.com>
2021-05-29 23:17:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ARCore Object Augmentation in Android Studio
+Basic introduction to ARCore in Android
+
+https://codemaker2015.medium.com/develop-your-helloar-app-in-android-studio-using-arcore-and-sceneform-d032e5788036
+
---
+++ app/src/main/java/com/example/arcorefirst/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.arcorefirst;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
+    ArFragment arFragment;
+    ModelRenderable lampPostRenderable;
+    private Uri selectedObject;
+
+    @Override
+    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!checkIsSupportedDeviceOrFinish(this))
+            return;
+
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitresult, Plane plane, MotionEvent motionevent) -> {
+                    if (plane.getType() != Plane.Type.HORIZONTAL_UPWARD_FACING)
+                        return;
+
+                    Anchor anchor = hitresult.createAnchor();
+                    placeObject(arFragment, anchor, R.raw.cube);
+                }
+        );
+    }
+
+    private void placeObject(ArFragment arFragment, Anchor anchor, int uri) {
+        ModelRenderable.builder()
+            .setSource(arFragment.getContext(), uri)
+            .build()
+            .thenAccept(modelRenderable -> addNodeToScene(arFragment, anchor, modelRenderable))
+            .exceptionally(throwable -> {
+                    Toast.makeText(arFragment.getContext(), "Error:" + throwable.getMessage(), Toast.LENGTH_LONG).show();
+                    return null;
+                }
+            );
+    }
+
+    private void addNodeToScene(ArFragment arFragment, Anchor anchor, Renderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+
+    private boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+}
